39badf5d552edaf923d56e7452af3e260078c90c
JonRider <jon@rider.net>
2019-08-20 11:13:51 +0530

    External js, pushing messages through socket.IO

--- application.py
+++ application.py
@@ -1,13 +1,20 @@
 import os
+import requests
 
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+messages = {"message": ""}
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
+
+@socketio.on("send message")
+def message(data):
+    messages["message"] = data["message"]
+    emit("recieve message", messages, broadcast=True)
---
+++ static/script.js
@@ -0,0 +1,138 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Connect to websocket
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+  // Send Message to Server
+  function sendMessage(message) {
+  // Emit send message
+    socket.emit('send message', {'message': message});
+  }
+
+  // Recieve the message from server
+  socket.on('recieve message', messages => {
+      addSent(messages.message)
+  });
+
+  // Point the default div
+  let def = document.querySelector('#default');
+  pointDiv(def);
+
+  // add a pointer and link to a div
+  function pointDiv(div) {
+    div.style.cursor = 'pointer';
+    div.onclick = function() {
+      window.location.href = 'www.google.com';
+    };
+  }
+
+  // Make add button a link
+  let add = document.querySelector('.add-box');
+  add.style.cursor = 'pointer';
+  // Get the add channel element
+  let addChannel = document.querySelector('#add-channel');
+
+
+  // Get new Channel
+  let newChannel = document.querySelector('#new-channel');
+  // Open Add Channel Input
+  add.onclick = function() {
+    // Make input field visible
+    if (newChannel.style.visibility == 'visible') {
+      newChannel.style.visibility = 'hidden';
+      // Empty Field
+      addChannel.value = '';
+      // Focus back on the message input
+      input.focus();
+      input.select();
+    }
+    else {
+      newChannel.style.visibility = 'visible';
+      // Focus on the addChannel input field
+      addChannel.focus();
+      addChannel.select();
+    }
+  };
+
+  // Get text for new channel from enter
+  addChannel.addEventListener('keydown', function(e) {
+    let channel = addChannel.value;
+    if (e.code === "Enter" && channel) {
+        // Make Channel
+        makeChannel(channel);
+        hideChannelInput();
+        }
+  });
+
+  // Get text for new channel from button
+  document.getElementById('add-channel-btn').addEventListener('click', function() {
+    let channel = addChannel.value;
+    if (channel) {
+        // Make Channel
+        makeChannel(channel);
+        hideChannelInput();
+        }
+  });
+
+  // Hide the input field and empty the values
+  function hideChannelInput() {
+    // Reset text field
+    addChannel.value = '';
+    newChannel.style.visibility = 'hidden';
+    // Focus back on the message input
+    input.focus();
+    input.select();
+  }
+
+
+  // Focus the Input Field on Intial Load
+  let input = document.getElementById('send');
+  input.focus();
+  input.select();
+  // Get input by enter key
+  input.addEventListener('keydown', function(e) {
+  let message = input.value;
+  if (e.code === "Enter" && message) {
+      // Send Message
+      //addSent(message);
+      sendMessage(message);
+      console.log(message);
+      // Reset text field
+      input.value = '';
+      }
+  });
+
+  function addSent(message) {
+    // Get message div
+    let div = document.getElementById('message-div');
+
+    // Build message bubble div
+    let bubble = document.createElement('div');
+    bubble.classList.add('message');
+    text = document.createElement('a');
+    text.innerHTML = message;
+    bubble.appendChild(text);
+    // Append message to div
+    div.appendChild(bubble);
+    linebreak = document.createElement("br");
+    div.appendChild(linebreak);
+    // Keep scrolled to bottom of messages
+    window.scrollTo(0,document.body.scrollHeight);
+  }
+
+  function makeChannel(channel) {
+    let div = document.getElementById('channel-div');
+    // Build channel tag
+    let tag = document.createElement('div');
+    tag.classList.add('channel-box');
+    heading = document.createElement('h5');
+    heading.classList.add('channels');
+    heading.innerHTML = '<i class="fa fa-hashtag"></i> ' + channel;
+    tag.appendChild(heading);
+    div.appendChild(tag);
+    // Get a pointer for the div
+    pointDiv(div);
+
+  }
+
+});
